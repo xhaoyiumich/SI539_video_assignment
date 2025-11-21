096aca7037d70c76b7e6c4523a78b01fab54300f
xhaoyiumich <xhaoyi@umich.edu>
2025-11-21 17:22:10 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -31,9 +31,29 @@ document.getElementById("faster").addEventListener("click",function() {
 	video.playbackRate += 0.1;
 })
 
-// document.getElementById("skip").addEventListener("click",function() {
-//     vid.pause();
-// })
+document.getElementById("mute").addEventListener("click",function() {
+    let vid = document.getElementById("player1");
+	if (vid.muted == true){
+		vid.muted = false;
+		document.getElementById("mute").textContent = "Mute"
+	}
+	else{
+		vid.muted = true;
+		document.getElementById("mute").textContent = "Unmute"
+	}
+	// document.getElementById("mute")
+})
+
+document.getElementById("skip").addEventListener("click",function() {
+    const videoElement = document.getElementById('player1');
+  	const skipAmount = 10;
+  	videoElement.currentTime += skipAmount;
+
+    if (videoElement.currentTime >= videoElement.duration) {
+        videoElement.currentTime = 0;
+    }
+	console.log(videoElement.currentTime)
+})
 
 document.getElementById("slider").addEventListener("input",function(){
 	let input = this.value;
